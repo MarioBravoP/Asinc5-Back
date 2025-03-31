b31c3b6b0fd288fdddeee46399f1f5454f82826e
Mario Bravo Pineda <135020003+MarioBravoP@users.noreply.github.com>
2025-03-31 12:02:49 +0200

    Update furnitures.js

--- routes/furnitures.js
+++ routes/furnitures.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const furnitureController = require('../controllers/furnitureController');
-const multer = require('multer');
 
 const upload = multer({ dest: 'uploadedFiles/' })
 
 router.get('/', furnitureController.getFurnitures);
-
-router.post('/create', upload.single('image') ,furnitureController.createFurniture)
+router.post('/create', furnitureController.createFurniture)
 
 module.exports = router
